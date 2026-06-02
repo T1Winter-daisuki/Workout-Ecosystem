b9439eb56526c486d88bbc48da768362ddb1dcaf
T1Winter-daisuki <mocchaneo@gmail.com>
2026-06-02 12:20:45 +0700

    Change from using gg mail to brevo to send otp

--- be/node-main-api/src/1_config/mailer.ts
+++ be/node-main-api/src/1_config/mailer.ts
@@ -1,32 +1,33 @@
-import nodemailer from 'nodemailer';
+import { BrevoClient } from '@getbrevo/brevo';
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-export const sendOTPEmail = async (to: string, otp: string) => {
+export const sendOTPEmail = async (
+  to: string,
+  otp: string,
+): Promise<boolean> => {
   try {
-    const info = await transporter.sendMail({
-      from: `"H4C App" <${process.env.EMAIL_USER}>`,
-      to,
-      subject: 'Mã xác thực OTP của H4C',
-      html: `
-        <h2>Mã OTP của bạn</h2>
-        <p>Mã OTP có hiệu lực trong <strong>3 phút</strong>:</p>
-        <h1 style="letter-spacing: 8px; color: #da4040;">${otp}</h1>
-        <p>Nếu bạn không yêu cầu mã này, hãy bỏ qua email này.</p>
-      `,
+    const brevo = new BrevoClient({
+      apiKey: process.env.BREVO_API_KEY as string,
     });
-    console.log('Email sent:', info.response);
+
+    const data = await brevo.transactionalEmails.sendTransacEmail({
+      subject: 'Mã xác thực OTP của H4C',
+      htmlContent: `
+        <div style="font-family: Arial, sans-serif; padding: 20px; text-align: center;">
+          <h2>Mã OTP của bạn</h2>
+          <p>Có hiệu lực trong <strong>3 phút</strong>:</p>
+          <h1 style="letter-spacing: 8px; color: #d64b29;">${otp}</h1>
+          <p>Nếu bạn không yêu cầu mã này, hãy bỏ qua email này.</p>
+        </div>
+      `,
+      sender: {
+        name: 'H4C App',
+        email: process.env.BREVO_SENDER_EMAIL as string,
+      },
+      to: [{ email: to }],
+    });
+
     return true;
-  } catch (error) {
-    console.error('Email error:', error);
+  } catch (err: any) {
     return false;
   }
 };
-
-export default transporter;
